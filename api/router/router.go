79a588abb8ac80792cb7f8bd55cece56a10deb68
Mr. D <dung13890@gmail.com>
2019-02-06 22:21:16 +0700

    jwt

--- api/router/router.go
+++ api/router/router.go
@@ -12,6 +12,12 @@ func Init(e *echo.Echo) {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 	user := &handler.User{}
+	e.POST("/login", handler.Login)
+
+	r := e.Group("/api")
+	r.Use(middleware.JWT([]byte("secret")))
+	r.GET("/info", handler.Restricted)
+
 	e.GET("/users", user.Index)
 	e.POST("/users", user.Create)
 	e.DELETE("/users/:id", user.Destroy)
